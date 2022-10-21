216154aa3a8e9f5d22302c7806b17ad6b72e6bde
fahh123 <ferrifabricio4@gmail.com>
2022-10-20 21:57:21 -0300

    projeto ecommerce rev.15

---
+++ js/map.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+    function init_map1() {
+        var myLocation = new google.maps.LatLng(-23.4866793,-47.4507891,16.75);
+        var mapOption = {
+            center:myLocation,
+            zoom:16
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "localização"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+        mapOptions);
+        marker.setMap(map);
+    }
+    init_map1();
+})
